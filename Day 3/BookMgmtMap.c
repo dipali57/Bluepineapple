615d6bf69522911913c9b86fc7e4892e578f945c
Dipali Gunjal <60472255+dipali57@users.noreply.github.com>
2021-12-20 21:50:57 +0530

    second commit

--- Day 3/BookMgmtMap.c
+++ Day 3/BookMgmtMap.c
@@ -34,21 +34,21 @@ int hashCode(char *str) {
 
 void Insert(char *value)
 {
-    //create a newnode with value
+   
     struct Book *newNode = malloc(sizeof(struct Book));
     strcpy(newNode->Name,value);
     newNode->next = NULL;
 
-    //calculate hash key
+    
     int key = hashCode(value);
 
-    //check if chain[key] is empty
+   
     if(array[key] == NULL)
         array[key] = newNode;
     //collision
     else
     {
-        //add the node at the end of chain[key].
+       
         struct Book *temp = array[key];
         while(temp->next)
         {
@@ -61,15 +61,20 @@ void Insert(char *value)
 int search(char *value)
 {
     int key = hashCode(value);
+    int i=0;
     struct Book *temp = array[key];
     while(temp!=NULL)
     {
-        printf("%s ",temp->Name);
-         printf("%s ",value);
-        if(strcmp(temp->Name,value)==0)
-            return 1;
+      
+        if(strcmp(temp->Name,value)==0){
+           printf("Book %s found at %d array position ",value,key);
+            return 1;  
+        }
+           
+        i++;
         temp = temp->next;
     }
+    
     return 0;
 }
 
@@ -107,18 +112,20 @@ int delete(char *value)
 
 void print()
 {
-    int i;
-
-    for(i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     {
+        
         struct Book *temp = array[i];
-        printf("array[%d]-->",i);
-        while(temp!=NULL)
-        {
-            printf("%s -->",temp->Name);
-            temp = temp->next;
-        }
-        printf("NULL\n");
+        if(temp!=0){
+             printf("array[%d]-->",i);
+            while(temp!=NULL)
+            {
+                printf("%s -->",temp->Name);
+                temp = temp->next;
+            }
+            printf("NULL");
+            printf("\n-----------------------------------------------------\n");
+        }     
     }
 }
 int main(){
@@ -127,47 +134,49 @@ int ch=0;
 
 char BName[20];
 printf("Book Management System !!\n");
-printf("\n 1.insert  \n 2.search Last \n 3.Delete First \n 4.Print \n 0.Exit \n \n ");
-init();
-		while(1){
-					printf("\n Enter your choice: ");
-					scanf("%d",&ch);
-						
-						switch(ch){
 
-							case 1:
-								printf("Enter Book Name: ");
-								scanf(" %[^\n]s",BName);
-								Insert(BName);
-								break;
-							case 2:
-								printf("Enter book Name you want to search: ");
-								scanf(" %[^\n]s",BName);
-								int ret=search(BName);
-                                if(ret==1){
-                                    printf("\nBook found");
-                                }
-                                else{
-                                    printf("\nBook Not found\n");
-                                }
-								break;
-						  	case 3:
-						  		printf("Enter book you want to delete: ");
-						  		scanf(" %[^\n]s",BName);
-								int ret1=delete(BName);
-                                    if(ret1==1)
-                                        printf("\n deleted successfully ");
-                                    else
-                                        printf("\n Book not found ");
-								break;
-							case 4:
-								print();
-								break;
+init();
+	while(1){
+
+        printf("\n 1.insert\n 2.search Last \n 3.Delete First \n 4.Print \n 0.Exit \n \n ");
+	printf("\n Enter your choice: ");
+	scanf("%d",&ch);
+						
+	switch(ch){
+
+		case 1:
+			printf("Enter Book Name: ");
+			scanf(" %[^\n]s",BName);
+			Insert(BName);
+			break;
+		case 2:
+			printf("Enter book Name you want to search: ");
+			scanf(" %[^\n]s",BName);
+			int ret=search(BName);
+                	if(ret==1){
+                    		printf("\nBook found\n");
+                	}
+                	else{
+                	    printf("\nBook Not found\n");
+        	        }
+			break;
+		case 3:
+			printf("Enter book you want to delete: ");
+			scanf(" %[^\n]s",BName);
+			int ret1=delete(BName);
+                	if(ret1==1)
+                		printf("\n deleted successfully ");
+                	else
+                		printf("\n Book not found ");
+			break;
+		case 4:
+			print();
+			break;
 					
-							case 0:
-								exit(0);
-								break;
-						}
+		case 0:
+			exit(0);
+			break;
+			}
 		}
 
 	return 0;
